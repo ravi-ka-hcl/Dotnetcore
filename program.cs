71e46cf7abd83ff10cb507d0588f94c5f66f09cd
ravi-ka-hcl <ravi-ka@hcltech.com>
2026-02-19 14:25:27 +0530

    Create program.cs

---
+++ program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace DotNetCoreApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello from .NET Core Console App!");
+        }
+    }
+}
